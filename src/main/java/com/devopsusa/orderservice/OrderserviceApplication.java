b9740ff3964c7c4e9d9d3504d7e9762d2bb5a019
nirajthanki6 <nirajthanki6@gmail.com>
2020-12-10 00:45:49 -0800

    Swagger UI Docket Configuration & Contact Information

--- src/main/java/com/devopsusa/orderservice/OrderserviceApplication.java
+++ src/main/java/com/devopsusa/orderservice/OrderserviceApplication.java
@@ -5,7 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @SpringBootApplication
 @ComponentScan("com.devopsusa.orderservice")
@@ -19,6 +26,29 @@ public class OrderserviceApplication {
 	@Bean
 	public RestTemplate restTemplate(){
 		return new RestTemplate();
+	}
+
+	@Bean
+	public Docket swaggerConfiguration() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.paths(PathSelectors.ant("/api/**"))
+				.apis(RequestHandlerSelectors.basePackage("com.devopsusa.orderservice"))
+				.build()
+				.apiInfo(apiDetails());
+	}
+
+	private ApiInfo apiDetails() {
+		return new ApiInfo(
+				"Order Service API",
+				"Service API for Placing Order",
+				"1.0",
+				"Free to Utilize",
+				new springfox.documentation.service.Contact("Niraj Thanki","http://devopsusa.com",
+						"nirajthanki6@gmail.com"),
+				"API License",
+				"http://devopsusa.com",
+				Collections.emptyList());
 	}
 
 }
