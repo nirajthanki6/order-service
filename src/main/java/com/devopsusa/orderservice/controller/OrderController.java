9879752a710463f73ab627b7cb20ed59621411e4
nirajthanki6 <nirajthanki6@gmail.com>
2020-12-09 23:43:47 -0800

    Implement Swagger UI at root

--- src/main/java/com/devopsusa/orderservice/controller/OrderController.java
+++ src/main/java/com/devopsusa/orderservice/controller/OrderController.java
@@ -11,19 +11,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/orders")
+@RequestMapping("/api")
 public class OrderController {
 
     @Autowired
     private OrderService orderService;
 
-    @PostMapping("/fruitOrder")
+    @PostMapping("/orders/fruitOrder")
     public ResponseEntity<TransactionResponse> fruitOrder(@RequestBody TransactionRequest payload){
         TransactionResponse transactionResponse = orderService.saveOrder(payload);
         return new ResponseEntity<TransactionResponse>(transactionResponse, new HttpHeaders(), HttpStatus.OK);
     }
 
-    @PutMapping("/fruitOrder/{id}")
+    @PutMapping("/orders/fruitOrder/{id}")
     public ResponseEntity<TransactionResponse> updateFruitOrder(@RequestBody TransactionRequest payload,
                                                                 @PathVariable Integer id){
         TransactionResponse transactionResponse = orderService.updateOrder(id,payload);
