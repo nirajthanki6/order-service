bf1f308d3c8a6533e2eaf8b2cf082b38d4970f72
nirajthanki6 <nirajthanki6@gmail.com>
2020-12-09 23:31:15 -0800

    add swagger for documenting API

--- src/main/java/com/devopsusa/orderservice/OrderserviceApplication.java
+++ src/main/java/com/devopsusa/orderservice/OrderserviceApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @ComponentScan("com.devopsusa.orderservice")
+@EnableSwagger2
 public class OrderserviceApplication {
 
 	public static void main(String[] args) {
